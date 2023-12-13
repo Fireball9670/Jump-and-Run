a9b400abae95c37643f13ea1a443be20760f6125
Fireball <fireball9670@gmail.com>
2023-12-13 17:24:52 +0100

    added jumping and fireballs

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var fireball_scene: PackedScene = preload("res://scenes/projectiles/fireball.tscn")
+
 var test_array: Array[String] = ["Test, Beans, Bruh"]
 
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +14,12 @@ func _process(_delta):
 
 func _on_gate_player_entered_gate(body):
 	print(body)
+
+
+
+
+func _on_player_fireball(pos, direction):
+	var fireball = fireball_scene.instantiate() as RigidBody2D
+	fireball.position = pos
+	fireball.linear_velocity = direction * 500
+	$Projectiles.add_child(fireball)
